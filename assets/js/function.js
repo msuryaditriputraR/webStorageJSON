917c481186a449dca1075df1c8073a7ab89dd546
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2021-06-09 02:09:12 +0800

    create function putUserList

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,5 +1,20 @@
+import storageKey from './key';
+
 const checkForStorage = () => {
     return typeof Storage !== 'undefined';
 };
 
-export { checkForStorage };
+const putUserList = data => {
+    if (checkForStorage()) {
+        let userData = [];
+        if (localStorage.getItem(storageKey) === null) userData = [];
+        else userData = JSON.parse(localStorage.getItem(storageKey));
+
+        userData.unshift(data);
+        if (userData.length > 5) userData.pop();
+
+        localStorage.setItem(storageKey, JSON.stringify(userData));
+    }
+};
+
+export { putUserList };
