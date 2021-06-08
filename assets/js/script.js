657d3bc9e5efb2f2830510360fa2d06c898d414a
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2021-06-09 02:35:39 +0800

    add eventLister to window when load document

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 import { submitAction } from './dom';
-import { submitActionListener } from './function';
+import { submitActionListener, windowLoadListener } from './function';
+
+window.addEventListener('load', windowLoadListener);
 
 submitAction.addEventListener('submit', submitActionListener);
